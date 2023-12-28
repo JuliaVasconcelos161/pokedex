3df68c5d8969f54bf1792dd293121132e073f282
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-12-28 20:31:14 -0300

    feat: dinamizando cores dependendo dos tipos de pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 function convertPokemonTypeToLi(pokemonTypes) {
-    return pokemonTypes.map((type) => `<li class="type">${type}</li>`);
+    return pokemonTypes.map((type) => `<li class="type ${type}">${type}</li>`);
 }
 
 function convertPokemonToLi(pokemon) {
     return `
-    <li class="pokemon ${pokemon.type}">
+    <li class="pokemon ${pokemon.mainType}">
         <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
