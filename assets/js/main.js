9eafce9d972e8f33f677a66327a8e536cf8c72f3
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-12-28 18:41:35 -0300

    feat: utilizando arrow function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,8 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?${offset}&${limit}`;
 
-fetch(url).then(function (response) {
-    return response.json();
-})
-.then(function (responseBody) {
-    console.log(responseBody);
-})
-.catch(function (error) {
-    console.error(error);
-}).finally(function () {
-    console.log('Requisicao concluida');
-});
+fetch(url)
+    .then((response) => response.json())
+    .then((responseBody) => console.log(responseBody))
+    .catch((error) => console.error(error))
+    .finally(() =>console.log('Requisicao concluida'));
